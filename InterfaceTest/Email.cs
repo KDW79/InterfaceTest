c5202fa473221e3f24944ad58ac1f9b7a2830b08
KDW79dev <42425701+KDW79@users.noreply.github.com>
2020-07-04 22:47:55 +0900

    초기 커밋.

---
+++ InterfaceTest/Email.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceTest
+{
+    public class Email : ISendable
+    {
+        private string email = "admin@c.com";
+        public void Send(string msg)
+        {
+            Console.WriteLine($"{msg} to {email}");
+        }
+    }
+
+    public class SnailMail : ISendable
+    {
+        public string Address { get; set; }
+
+        public void Send(string msg)
+        {
+            Console.WriteLine($"Visit {Address} and deliver note: {msg}");
+        }
+
+        public bool CheckAddress()
+        {
+            // Check...
+            return true;
+        }
+    }
+
+    public class SMS : ISendable
+    {
+        public string Phone { get; set; }
+
+        public void Send(string msg)
+        {
+            Console.WriteLine($"Send message {msg} to {Phone}");
+
+        }
+
+        public void TestPhoneNumber()
+        {
+            //...
+
+        }
+    }
+
+
+}
